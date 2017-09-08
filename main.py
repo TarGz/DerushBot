00cebe8085a0e7d768be5a5fd1ecda78849d3173
Julien Terraz <julien@terraz.org>
2017-09-08 11:21:28 +0200

    First working POC

--- main.py
+++ main.py
@@ -1,70 +1,72 @@
-print("Derushbot V0.0.1")
+print("Derushbot V0.0.2")
 
 from moviepy.editor import VideoFileClip, ImageClip
 import datetime
-#clip = VideoFileClip("sample.mp4")
-clip = VideoFileClip("sample2.MP4")
-gap=3
-fps= 1/gap
-cduration=30
-nframes = clip.duration*fps # total number of frames used
-print(nframes)
-#total_image = sum(clip.iter_frames(fps,dtype=float,progress_bar=True))
+import glob
+import os.path
 
-# for frame in clip.iter_frames(fps,dtype=float,progress_bar=True):
-# 	# print(frame[0,:,0])
-# 	# print(frame[0,:,0].max())
-# 	print(frame[0,:,0])
-# 	print(frame[1,:,1])
-# 	print(frame[2,:,2])
-
-
-#print("Total image : {}".format(total_image))
-#1average_image = ImageClip(total_image/ nframes)
-#average_image.save_frame("output.png")
+folder_todo = "TODO/"
+folder_done = "DONE/"
 
 
 
-i=0
-blackframe=0
-for frame in clip.iter_frames(fps,dtype=int,progress_bar=False):
-	i = i + 1
-	# print(frame[0,:,0])
-	# print(frame[0,:,0].max())
-	red = frame[400,:,0].max();
-	# print(red)
-	green = frame[400,:,1].max();
-	# print(green.sum())
-	blue = frame[400,:,2].max();
-	# print(blue)
-	average=int((red+green+blue)/3)
-	time = i / fps
-	timestamp = str(datetime.timedelta(seconds=time))
+def getFileList(path):
+	print(path)
+	todo = glob.glob(path+folder_todo+"*.MP4")
+	print("%s MP4 files found in %s " % (len(todo),path+folder_todo))
+	for vid in todo:
+		print(vid)
+		finename = os.path.basename(vid)
+		print(finename)
+		dest = path+folder_done+finename
+		print(dest)
+		seekBlackFrame(finename,path)
+		os.rename(vid,dest)
+
+
+def seekBlackFrame(videofile,path,gap=3,cduration=120):
 	
-	
-	if(average>0):
-		
-		if(blackframe>0):
-			blackframe=0
-			clipbegin = time-cduration-gap
-			if(clipbegin < 0): clipbegin = 0
-			clipend = time-(gap*2)
-			print("Saving sublcip")
-			print("clipbegin"+str(clipbegin))
-			print("clipend"+str(clipend))
+	clip = VideoFileClip(path+folder_todo+videofile)
+	fps= 1/gap
+	nframes = clip.duration*fps # total number of frames used
+	print("seekBlackFrame %s as %s frames" % (videofile, nframes))
 
-			new_filename = "sample2->%s->%s.mp4" % (str(clipbegin),str(clipend))
-			newclip = clip.subclip(clipbegin,clipend)
-			newclip.write_videofile(new_filename)
+	i=0
+	blackframe=0
+	for frame in clip.iter_frames(fps,dtype=int,progress_bar=False):
+		i = i + 1
 
-		else:
-			print("searching at "+timestamp+"s :"+str(average))
+		red = frame[400,:,0].max();
+		green = frame[400,:,1].max();
+		blue = frame[400,:,2].max();
+		average=int((red+green+blue)/3)
+		time = i / fps
+		timestamp = str(datetime.timedelta(seconds=time))
 
-	elif(average == 0):
-		blackframe=blackframe+1
-		print("blackframe found at "+str(timestamp)+"s :"+str(average))
+		if(average>0):
+			
+			if(blackframe>0):
+				blackframe=0
+				clipbegin = time-cduration-gap
+				if(clipbegin < 0): clipbegin = 0
+				clipend = time-(gap*2)
+				print("Saving sublcip")
+				print("clipbegin"+str(clipbegin))
+				print("clipend"+str(clipend))
+
+				new_filename = "%s%s->%s->%s.mp4" % (path,videofile,str(clipbegin),str(clipend))
+				newclip = clip.subclip(clipbegin,clipend)
+				newclip.write_videofile(new_filename)
+
+			else:
+				print("searching at "+timestamp+"s :"+str(average))
+
+		elif(average == 0):
+			blackframe=blackframe+1
+			print("blackframe found at "+str(timestamp)+"s :"+str(average))
 
 
 
-
+#seekBlackFrame("FILE0404.MP4","/Users/jterraz/Desktop/DERUSH/",3)
+getFileList("/Users/jterraz/Desktop/DERUSH/")
 
