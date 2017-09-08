b91f714662c01b7e08f727ddaa211c295dfa13e9
Julien Terraz <julien@terraz.org>
2017-09-08 20:59:37 +0200

    Managing files and folders

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
-print("Derushbot V0.0.2")
+
 
 from moviepy.editor import VideoFileClip, ImageClip
 import datetime
+import sys
+import time
 import glob
 import os.path
 
 folder_todo = "TODO/"
 folder_done = "DONE/"
 
-
+def createfolderifneeded(path):
+	todo = path+folder_todo
+	if not os.path.exists(todo):
+		print("creating folder : " + todo)
+		os.makedirs(todo)
+	done = path+folder_done
+	if not os.path.exists(done):
+		print("I'm creating the folder : " + done)
+		print("put your MP4 files inside")
+		os.makedirs(done)
 
 def getFileList(path):
 	print(path)
@@ -18,9 +29,10 @@ def getFileList(path):
 		print(vid)
 		finename = os.path.basename(vid)
 		print(finename)
-		dest = path+folder_done+finename
+		repport = seekBlackFrame(finename,path)
+		dest = path+folder_done+repport+"-"+finename
 		print(dest)
-		seekBlackFrame(finename,path)
+		
 		os.rename(vid,dest)
 
 
@@ -33,6 +45,7 @@ def seekBlackFrame(videofile,path,gap=3,cduration=120):
 
 	i=0
 	blackframe=0
+	report="minoritaire"
 	for frame in clip.iter_frames(fps,dtype=int,progress_bar=False):
 		i = i + 1
 
@@ -40,33 +53,64 @@ def seekBlackFrame(videofile,path,gap=3,cduration=120):
 		green = frame[400,:,1].max();
 		blue = frame[400,:,2].max();
 		average=int((red+green+blue)/3)
-		time = i / fps
-		timestamp = str(datetime.timedelta(seconds=time))
+		t = i / fps
+		timestamp = str(datetime.timedelta(seconds=t))
 
 		if(average>0):
 			
 			if(blackframe>0):
 				blackframe=0
-				clipbegin = time-cduration-gap
+				report="majoritaire"
+				clipbegin = t-cduration-gap
 				if(clipbegin < 0): clipbegin = 0
-				clipend = time-(gap*2)
+				clipend = t-(gap*2)
 				print("Saving sublcip")
-				print("clipbegin"+str(clipbegin))
-				print("clipend"+str(clipend))
+				print("clipbegin : "+str(clipbegin))
+				print("clipend : "+str(clipend))
+				clipbegin_str = time.strftime("%M%S", time.gmtime(clipbegin))
+				clipend_str = time.strftime("%M%S", time.gmtime(clipend))
 
-				new_filename = "%s%s->%s->%s.mp4" % (path,videofile,str(clipbegin),str(clipend))
+
+				print("clipbegin_str:"+clipbegin_str)
+				print("clipend_str:"+clipend_str)
+				
+				
+
+				begin_time = datetime.timedelta(seconds=clipbegin)
+				new_filename = "%s%s-%s->%s.MP4" % (path,videofile,clipbegin_str,clipend_str)
 				newclip = clip.subclip(clipbegin,clipend)
-				newclip.write_videofile(new_filename)
+				newclip.write_videofile(new_filename,
+					codec='libx264', 
+					audio_codec='aac', 
+					temp_audiofile='temp-audio.m4a', 
+					remove_temp=True)
 
 			else:
-				print("searching at "+timestamp+"s :"+str(average))
+				print(videofile+" searching at "+timestamp+"s Max level :"+str(average))
 
 		elif(average == 0):
 			blackframe=blackframe+1
 			print("blackframe found at "+str(timestamp)+"s :"+str(average))
 
+	return 	report
 
 
-#seekBlackFrame("FILE0404.MP4","/Users/jterraz/Desktop/DERUSH/",3)
-getFileList("/Users/jterraz/Desktop/DERUSH/")
+# getFileList("/Users/jterraz/Desktop/DERUSH/")
+
+print("Derushbot V0.0.2")
+
+if len(sys.argv) > 1:
+	folder_arg = sys.argv[1]
+	if(os.path.isdir(folder_arg)):
+		createfolderifneeded(folder_arg)
+		getFileList(folder_arg)
+	else:
+		print(folder_arg + " is not a valid path")
+else:
+    print("you should provide a path")  
+
+
+
+
+
 
